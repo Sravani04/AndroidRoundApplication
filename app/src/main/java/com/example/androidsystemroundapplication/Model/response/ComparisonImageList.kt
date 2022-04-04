34c2846836a84a6a5174fe53800a4ec62ee400ce
Sravani <sravani@fishingchimes.com>
2022-04-04 10:37:16 +0530

    initial commit

---
+++ app/src/main/java/com/example/androidsystemroundapplication/Model/response/ComparisonImageList.kt
@@ -0,0 +1,10 @@
+package com.example.androidsystemroundapplication.Model.response
+
+import java.text.FieldPosition
+
+class ComparisonImageList(
+
+     var id: Int? = null,
+     var title: String? = null,
+     var url: String? = null
+ )
